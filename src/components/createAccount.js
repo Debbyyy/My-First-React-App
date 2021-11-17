c1df2d4fba7e46047275296d39fbe9cf95308ca0
Dhebby <eldhebbyjay609@gmail.com>
2021-11-17 08:59:24 -1100

    progress: learn app

--- src/components/createAccount.js
+++ src/components/createAccount.js
@@ -2,58 +2,60 @@ import React from 'react';
 import '../createAccount.css';
 
 class NewAccount extends React.Component {
-    /*state = {
-        username: ''
-    }
-    onUsernameChange = (e) => {
-        this.setState({
-            username: e.target.value
-        });
+    state = {
+        open: false
+    };
+    handleClick = () => {
+        this.setState((state) => {
+            return {
+                open: !state.open
+            }
+        })
     }
     handleSubmit = (e) => {
         e.preventDefault();
         console.log('user submitted');
         this.props.history.push('/profile')
-    }*/
+    }
     render() {
-    return(
-        // return(
-        <div className="m-14 md:m-20 flex-col md:items-center md:justify-items-center">
-            <h1 className="text-2xl font-bold mb-10 text-center ">Create Your Account</h1>
-            {/* onSubmit={handleSubmit(handleSubmit)} */}
-            <form className="flex flex-col items-center justify-items-center"
-            onSubmit={this.handleSubmit}>
-                <label htmlFor="First name">
-                    <input type="text" placeholder="First name" id="fname"
-                    // value={this.state.username} onChange={this.onUsernameChange}
-                        className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
-                </label>
-                {/* {this.state.fnameError ? this.state.fnameError : null} */}
+        return (
+            // return(
+            <div className="m-14 md:m-20 flex-col md:items-center md:justify-items-center">
+                <h1 className="text-2xl font-bold mb-10 text-center">Create Your Account</h1>
+                {/* onSubmit={handleSubmit(handleSubmit)} */}
+                <form className="flex flex-col items-center justify-items-center"
+                    onSubmit={this.handleSubmit}>
+                    <label htmlFor="First name">
+                        <input type="text" placeholder="First name" id="fname"
+                            // value={this.state.username} onChange={this.onUsernameChange}
+                            className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
+                    </label>
+                    {/* {this.state.fnameError ? this.state.fnameError : null} */}
 
-                <label htmlFor="Last name">
-                    <input type="text" placeholder="Last name" id="lname"
-                        className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
-                </label>
+                    <label htmlFor="Last name">
+                        <input type="text" placeholder="Last name" id="lname"
+                            className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
+                    </label>
 
-                <label htmlFor="Email">
-                    <input type="text" placeholder="xyz@example.com" id="email"
-                        className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
-                </label>
-                <label htmlFor="Password">
-                    <input type="password" placeholder="Choose a password" id="pword"
-                        className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
-                </label>
-
-                <label htmlFor="Gender">
-                    <input type="text" placeholder="Gender" id="gender" 
-                        className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" />
-                </label>
-
-                <button id="btn"
-                    className="outline-none rounded-xl font-semibold mt-20 py-2 px-24 md:px-14">Submit</button>
-            </form>
-        </div>
-    );
+                    <label htmlFor="Email">
+                        <input type="text" placeholder="xyz@example.com" id="email"
+                            className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
+                    </label>
+                    <label htmlFor="Password">
+                        <input type="password" placeholder="Choose a password" id="pword"
+                            className="outline-none border border-gray-400 pr-40 pl-5 py-2 mb-5" required />
+                    </label>
+                    <label htmlFor="Gender" className="-ml-56">
+                        <input type="radio" name="sex" value="male" checked /> Male<br />
+                        <input type="radio" name="sex" value="male" /> Female<br />
+                        <input type="radio" name="sex" value="male" /> Prefer not to say
+                    </label>
+                    <button onSubmit={this.handleSubmit}
+                        className="bg-black text-white outline-none rounded-xl font-semibold mt-20 py-2 px-24 md:px-14">Submit
+                    </button>
+                </form>
+            </div >
+        );
+    }
 }
-}
-export default NewAccount 
+export default NewAccount
--- src/components/profile.js
+++ src/components/profile.js
@@ -5,10 +5,17 @@ import Food2 from '../images/food.jpg';
 import '../profile.css';
 
 class MyProfile extends Component {
+    state = {
+        open: false
+    };
+    handleClick = () => {
+        this.setState((state) => {
+            return {
+                open: !state.open
+            }
+        })
+    }
     render() {
-        // console.log('debbie', this.context);
-        // const { username } = this.context;
-        // const name = username ? username : (console.log('please add your name'));
         return (
             <div className="text-white flex flex-col font-bold bg-gray-800 p-10 bg-none">
                 <header className="flex flex-row justify-between">
@@ -26,18 +33,30 @@ class MyProfile extends Component {
                 <section>
                     <div>
                         <div className="bg-black rounded-xl py-4 px-2 md:mr-96 mb-10 mt-5 text-white">
-                            <input type="text" placeholder="Search our collection" className="bg-black"/>
+                            <input type="text" placeholder="Search our collection" className="bg-black" />
                             {/* add a search icon */}
                         </div>
 
                         <div>
-                            <ul className="flex flex-row md:mx-32 flex-wrap md:flex-nowrap mb-5 md:mb-10 md:justify-between">
+                            <ul className="hidden md:flex md:flex-row mr-36 md:mx-32 flex-nowrap mb-5 md:mb-10 md:justify-between">
                                 <li className="bg-black px-3 md:px-5 py-2 rounded-xl mr-2 text-sm md:text-base">New Entry</li>
                                 <li className="bg-black px-3 md:px-5 py-2 rounded-xl mr-2 text-sm md:text-base">African Dishes</li>
                                 <li className="bg-black px-3 md:px-5 py-2 rounded-xl mr-2 text-sm md:text-base">Mexican Dishes</li>
                                 <li className="bg-black px-3 md:px-5 py-2 rounded-xl mr-2 text-sm md:text-base">Chinese Dishes</li>
                                 <li className="bg-black px-3 md:px-5 py-2 rounded-xl text-sm md:text-base">Japanese Dishes</li>
                             </ul>
+
+                            <div className="sm:flex md:hidden inline-block relative">
+                                <button className="outline-none rounded-xl text-sm py-2 px-4 md:px-14 bg-black m-0" id="sm-screen" onClick={this.handleClick}>New Entry</button>
+                                {this.state.open && (
+                                    <div className="top-full absolute bg-yelllow-300" id="sm-screen-content">
+                                        <a href="#" className="float-none text-white px-2 py-4 no-underline block text-left">African Dishes</a>
+                                        <a href="#" className="float-none text-white px-2 py-4 no-underline block text-left">Mexican Dishes</a>
+                                        <a href="#" className="float-none text-white px-2 py-4 no-underline block text-left">Chinese Dishes</a>
+                                        <a href="#" className="float-none text-white px-2 py-4 no-underline block text-left">Japanese Dishes</a>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
 
@@ -53,7 +72,7 @@ class MyProfile extends Component {
                         <p className="mb-2 text-gray-500">This might interest you</p>
                         <div className="bg-black p-14 md:p-20 relative rounded-3xl">
                             <p>Spaghetti recipe</p>
-                            <img src={Food1} alt="a food" className="second-image " />
+                            <img src={Food1} alt="a food" className="second-image" id="sec-img" />
                         </div>
                     </div>
                 </section>
